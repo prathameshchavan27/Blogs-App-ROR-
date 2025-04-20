bbdf4f7b19d7abba69d72d1b02def3e3ac46b932
prathameshchavan27 <prathameshlchavan@gmail.com>
2025-04-20 16:59:17 +0530

    Fix RuboCop style violations

--- db/migrate/20250420110050_add_password_digest_to_users.rb
+++ db/migrate/20250420110050_add_password_digest_to_users.rb
@@ -1,6 +1,6 @@
 class AddPasswordDigestToUsers < ActiveRecord::Migration[8.0]
   def change
     add_column :users, :password_digest, :string
-    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+    # Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
   end
 end
