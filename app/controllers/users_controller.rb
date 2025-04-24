c9acef004fb287a9bbee32471e1d84a8077a877c
prathameshchavan27 <prathameshlchavan@gmail.com>
2025-04-24 10:10:35 +0530

    Add pagination to users and articles page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     end
 
     def index
-        @users = User.all
+        @users = User.paginate(page: params[:page], per_page:2)
     end
 
     def update
