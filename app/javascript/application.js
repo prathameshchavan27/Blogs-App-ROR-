09993f382dcc3a1401e9c1d72d0c8606ce7f920e
prathameshchavan27 <prathameshlchavan@gmail.com>
2025-04-25 00:16:08 +0530

    Add authentication and session for login

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
 import "@hotwired/turbo-rails"
-import Rails from "@rails/ujs"
-Rails.start()
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = true
 
 import "controllers"
+
