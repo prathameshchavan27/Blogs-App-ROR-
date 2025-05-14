2a9d116511bfb767a0c8e560589eb5398e86415c
prathameshchavan27 <prathameshlchavan@gmail.com>
2025-05-14 19:53:23 +0530

    Add article_category model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_02_095433) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_13_140652) do
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
